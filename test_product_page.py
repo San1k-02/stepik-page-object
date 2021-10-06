1120da854263419d332f68c7a790aaad07f6f5f9
San1k-02 <fizika10a2@yandex.ru>
2021-10-06 14:04:36 +0300

    fixed task_4_3_10

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link="http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page=ProductPage(browser,link)
     page.open()
